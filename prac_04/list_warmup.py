510471c87e6f3476538b04cf32a3ead725ac020d
FlynnCharlwood <116258134+FlynnCharlwood@users.noreply.github.com>
2022-10-20 19:21:20 +1000

    Add files via upload

---
+++ prac_04/list_warmup.py
@@ -0,0 +1,21 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0]
+numbers[-1]
+numbers[3]
+numbers[:-1]
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3]
+
+# Write Python expressions (in your Python file) to achieve the following:
+
+numbers[0] = "ten"
+
+numbers[-1] = 1
+
+numbers[2:]
+
+9 in numbers
